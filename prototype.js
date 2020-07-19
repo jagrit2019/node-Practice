8788a72bac31497f0e7a07bea083e3dc12e8df6c
JArit20 <jagritacharya2020@gmail.com>
2020-07-19 14:48:08 +0530

    http module intro,nodemon installed

--- prototype.js
+++ prototype.js
@@ -4,15 +4,25 @@ var student=function(){
     this.age= 12;
     this.email="jagritachaya2019@gmail.com"
 }
+// second way to create a function
+ function details(){
+this.name="Acharya";
+this.age=22;
+}
+
 student.prototype={
     address : "jaipur",
     getName:function(){
         return this.address;
     }
 }
+//object creating
+var deta = new details();
 var stud = new student();
 
 console.log(stud.name);
 console.log(stud.email);
 // console.log(stud.address);
 console.log(stud.getName());
+console.log(deta.name)
+console.log(deta.age);
