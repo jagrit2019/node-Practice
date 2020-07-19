fdfcd51267abca0602c4037311813af810adf00b
JArit20 <jagritacharya2020@gmail.com>
2020-07-19 11:02:15 +0530

    objects create

---
+++ objects.js
@@ -0,0 +1,6 @@
+console.log("hello world jagrit was here ")
+var users={name:"jagrit here", year:"2",college:"NMIMS",future:"CEO"}
+console.log(users)
+console.log(users.name)
+console.log(users.future)
+console.log(users.year)
