0b7a0db23079377424d62a0ea9252ac6868834aa
JArit20 <jagritacharya2020@gmail.com>
2020-07-19 11:21:19 +0530

    functions added

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 var person=function(){
     console.log("this is a function")
 }
-//parametrised function
+//parametrised anoynomous function
 
 var calculator=function( a, b){
     console.log(a+b)
@@ -9,3 +9,16 @@ var calculator=function( a, b){
 }
 person();
 calculator(9,3)
+
+// arrow function
+var arrow = ()=>{
+console.log("this is the hell of the arrow function")
+}
+
+var valueArrow=(a,b)=>{
+    var c = a+b;
+    console.log(c);
+    
+}
+arrow()
+valueArrow(343+32453)
