82646bc05054afd195bfb3398210a80bd2a47d7a
JArit20 <jagritacharya2020@gmail.com>
2020-07-19 11:09:34 +0530

    function created

---
+++ functions.js
@@ -0,0 +1,11 @@
+var person=function(){
+    console.log("this is a function")
+}
+//parametrised function
+
+var calculator=function( a, b){
+    console.log(a+b)
+    console.log(a-b)
+}
+person();
+calculator(9,3)
