d77a223dff99a98d9021bc2c91cd8a53a3807291
JArit20 <jagritacharya2020@gmail.com>
2020-07-19 20:26:17 +0530

    written and read with a call back hell

--- read.js
+++ read.js
@@ -1,8 +1,42 @@
 const fs = require("fs");
 
-const textin= fs.readFileSync("./1-node-farm/starter/txt/input.txt","utf-8")
-console.log(textin);
+//blocking synchronous way
 
-const textOut = `This is what i know about avocardo: ${textin}.\n Created on ${Date.now()}`;
-fs.writeFileSync('./output.txt',textOut);
-console.log("file written")
+// const textin= fs.readFileSync("./1-node-farm/starter/txt/input.txt","utf-8")
+// console.log(textin);
+
+// const textOut = `This is what i know about avocardo: ${textin}.\n Created on ${Date.now()}`;
+// fs.writeFileSync('./output.txt',textOut);
+// console.log("file written")
+
+//Non-blocking Asynchronous way
+// fs.readFile("./1-node-farm/starter/txt/start.txt",'utf-8',(err,text)=>{
+//     if (err) throw err;
+// console.log(text);
+// });
+// console.log("Will read this");
+// this is a callback hell
+fs.readFile('./1-node-farm/starter/txt/start.txt','utf-8',(err,data1)=>{
+    fs.readFile(`./1-node-farm/starter/txt/${data1}.txt`,'utf-8',(err,data2)=>{
+        if (err ) throw err;
+        console.log(data2);
+
+        fs.readFile('./1-node-farm/starter/txt/append.txt','utf-8',(err,data3)=>{
+            if(err) throw err;
+            console.log(data3);
+            fs.writeFile('./1-node-farm/starter/txt/merged.txt',`${data2}\n${data3}`,'utf-8',err =>{
+                if(err) throw err;
+                console.log("Your file has been written !!")
+
+            })
+        });
+       
+    });
+
+});
+console.log("will read this no issues")
+
+// old fasion function
+// function name(params) {
+    
+// }
