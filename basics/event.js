47b917b00508aaf0ae0bbfb9a6c7bffdbad64cb5
jagrit2019 <53462496+jagrit2019@users.noreply.github.com>
2020-10-01 01:00:57 +0530

    Update event.js

--- basics/event.js
+++ basics/event.js
@@ -1,6 +1,6 @@
 const events = require("events");
 const event = new events.EventEmitter();
 
-event.on('click',()=>{
+event.on('click',()=>{.          // this way was introduced in ES6
     console.log("event create")
 });
